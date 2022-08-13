f6a16411e92fbb3b7684a4187bec7a951c4ba346
Ngo Huu Tri <s3818520@rmit.edu.vn>
2022-08-13 18:34:40 +0700

    Initial Commit

---
+++ ArknightsGachaSim/ArknightsGachaSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArknightsGachaSimApp.swift
+//  ArknightsGachaSim
+//
+//  Created by Huu Tri on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArknightsGachaSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
